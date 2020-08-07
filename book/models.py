8e08d442c5a108621410e64263af9b1410a6987f
ibrahimokumus_09@hotmail.com <ibrahimokumus_09@hotmail.com>
2020-08-07 14:59:24 +0300

    third commit

--- book/models.py
+++ book/models.py
@@ -1,3 +1,69 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+from mptt.fields import TreeForeignKey
+from mptt.models import MPTTModel
+
+
+class Category(MPTTModel):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(blank=True, max_length=255)
+    description = models.CharField(blank=True, max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    status = models.CharField(max_length=10, choices=STATUS)
+    slug = models.SlugField(null=False, unique=True)
+    parent = TreeForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['title']
+
+    def __str__(self):  # __str__ method elaborated later in
+        full_path = [self.title]  # post.  use __unicode__ in place of
+        k = self.parent
+        while k is not None:
+            full_path.append(k.title)
+            k = k.parent
+        return ' / '.join(full_path[::-1])
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+
+class Book(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    #user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)  # relation with Category table
+    title = models.CharField(max_length=150)
+    keywords = models.CharField(blank=True, max_length=255)
+    description = models.CharField(blank=True, max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.CharField(max_length=250)
+    slug = models.SlugField(null=False, unique=True)
+    status = models.CharField(max_length=10, choices=STATUS)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author=models.CharField(max_length=50)
+    publishDate=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+
+
+    image_tag.short_description = 'Image'
